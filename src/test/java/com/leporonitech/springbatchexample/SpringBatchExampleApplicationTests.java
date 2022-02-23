ea0976416d647501756290feffe53360c89c2901
Leporoni <leporoni@hotmail.com>
2022-02-23 16:01:09 -0300

    Created project and config application.yml to use mysql database

---
+++ src/test/java/com/leporonitech/springbatchexample/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.springbatchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
