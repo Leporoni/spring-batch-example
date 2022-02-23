f0a4c235edeea4dbf91a095dc358fa25d94f521e
Leporoni <leporoni@hotmail.com>
2022-02-23 16:16:55 -0300

    Created the aircraft model and added the necessary annotations

---
+++ src/main/java/com/leporonitech/springbatchexample/Aircraft.java
@@ -0,0 +1,29 @@
+package com.leporonitech.springbatchexample;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+public class Aircraft implements Serializable {
+
+    private static final long serialVersionUID = 9058350955651091145L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String recordKey;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String iataCode;
+
+    private String icaoCode;
+}
